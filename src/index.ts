6105c2834acdf054ed36de748552b4ef2d2cd03b
pranavpawarr <pranavpawar503@gmail.com>
2025-11-15 01:13:15 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import { Request, Response } from "express";
+
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export async function handlerReadiness(req: Request, res: Response) {
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send("OK");
-}
+app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
-app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
