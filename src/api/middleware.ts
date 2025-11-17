8dc5675bb147bafbaafb65c94f15d63e490f3958
pranavpawarr <pranavpawar503@gmail.com>
2025-11-18 01:05:06 +0530

    admin namespace set

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -35,8 +35,13 @@ export function middlewareMetricsInc(
 }
 
 export function handlerMetrics(_req: Request, res: Response) {
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send(`Hits: ${configobj.fileserverHits}`);
+  res.set("Content-Type", "text/html; charset=utf-8");
+  res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${configobj.fileserverHits} times!</p>
+  </body>
+</html>`);
 }
 
 export function handlerReset(_req: Request, res: Response) {
