690e5da217c6fca6a4f78f5427a79b1118f1b432
pranavpawarr <pranavpawar503@gmail.com>
2025-11-22 16:45:54 +0530

    jwt

---
+++ src/api/auth.test.ts
@@ -0,0 +1,55 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { makeJWT, validateJWT } from "../api/auth.js";
+import { hashPassword, checkPasswordhash } from "./auth";
+
+describe("JWT Tests", () => {
+  const secret = "testSecret";
+  const userID = "user123";
+  let token: string;
+
+  it("should create a valid JWT", () => {
+    token = makeJWT(userID, 60, secret);
+    expect(typeof token).toBe("string");
+  });
+
+  it("should validate a valid JWT and return the user ID", () => {
+    const sub = validateJWT(token, secret);
+    expect(sub).toBe(userID);
+  });
+
+  it("should reject an expired JWT", () => {
+    const expiredToken = makeJWT(userID, -1, secret);
+    expect(() => validateJWT(expiredToken, secret)).toThrow(
+      "Invalid or expired token"
+    );
+  });
+
+  it("should reject JWTs signed with the wrong secret", () => {
+    const tokenWithWrongSecret = makeJWT(userID, 60, "wrongSecret");
+    expect(() => validateJWT(tokenWithWrongSecret, secret)).toThrow(
+      "Invalid or expired token"
+    );
+  });
+});
+
+describe("Password Hashing", () => {
+  const password1 = "correctPassword123!";
+  const password2 = "anotherPassword456!";
+  let hash1: string;
+  let hash2: string;
+
+  beforeAll(async () => {
+    hash1 = await hashPassword(password1);
+    hash2 = await hashPassword(password2);
+  });
+
+  it("should return true for the correct password", async () => {
+    const result = await checkPasswordhash(password1, hash1);
+    expect(result).toBe(true);
+  });
+
+  it("should return false for incorrect password", async () => {
+    const result = await checkPasswordhash(password2, hash1);
+    expect(result).toBe(false);
+  });
+});
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,7 @@
 import argon2 from "argon2";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
 
 export async function hashPassword(password: string): Promise<string> {
   try {
@@ -24,5 +27,31 @@ export async function checkPasswordhash(
   } catch (err) {
     console.log(err);
     return false;
+  }
+}
+
+export function makeJWT(
+  userID: string,
+  expiresIn: number,
+  secret: string
+): string {
+  const iat = Math.floor(Date.now() / 1000);
+
+  const payload: Payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat,
+    exp: iat + expiresIn,
+  };
+
+  return jwt.sign(payload, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    const payload = jwt.verify(tokenString, secret) as Payload;
+    return payload.sub!;
+  } catch (err) {
+    throw new Error("Invalid or expired token");
   }
 }
